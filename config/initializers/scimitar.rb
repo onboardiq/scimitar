da07d2a83d8f8d855e55def982dc51fcb11800da
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2022-01-20 10:52:16 +1300

    Work around autoloader issues

--- config/initializers/scimitar.rb
+++ config/initializers/scimitar.rb
@@ -2,6 +2,8 @@
 #
 # For supporting information and rationale, please see README.md.
 
+Rails.application.config.to_prepare do # (required for >= Rails 7 / Zeitwerk)
+
 # =============================================================================
 # SERVICE PROVIDER CONFIGURATION
 # =============================================================================
@@ -100,3 +102,5 @@ Scimitar.engine_configuration = Scimitar::EngineConfiguration.new({
   #
   #     optional_value_fields_required: false
 })
+
+end
